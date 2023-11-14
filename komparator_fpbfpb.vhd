76fb5e851d9e53d67c8fcfd5409488e5a75ea2f2
Klovinki <150678581+Klovinki@users.noreply.github.com>
2023-11-14 13:50:43 +0700

    Update and rename komparator_kpk.vhd to komparator_fpbfpb.vhd

--- komparator_fpbfpb.vhd
+++ komparator_fpbfpb.vhd
@@ -3,14 +3,14 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_arith.all;  
 use IEEE.std_logic_unsigned.all;  
  
-entity komparator_kpk is  port(  
+entity komparator_fpb is  port(  
 	a, b: in std_logic_vector( 31 downto 0 );  
 	a_eq_b	: out std_logic;
 	a_lowerThan_b	: out std_logic
 	);
-end komparator_kpk;  
+end komparator_fpb;  
  
-architecture comparator_arc of komparator_kpk 
+architecture comparator_arc of komparator_fpb 
 is  begin  process( a, b)  begin  
 	if( a < b ) then  
 		a_eq_b <= '0'; -- if a < b  
